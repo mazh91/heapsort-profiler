3f7c436b971166478daf78bdfcfffee4c149884e
mazh91 <milad.azh@ryerson.ca>
2017-03-10 02:45:14 -0500

    Update Assignment to be instantiatable*

--- Assignment.java
+++ Assignment.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 
 public class Assignment {
-	final static int N = 100;
-	final static int MAXVALUE = 10*N;
-	final static int AVERAGEOVER = 1000000 / N;
-	public static void main(String[] args) 
+//	final static int N = 100;
+//	public static void main(String[] args)
+	public Assignment(int N, Random random)
 	{
-	    Random random = new Random();
+		final int MAXVALUE = 10*N;
+		final int AVERAGEOVER = 1000000 / N;
 	
 	    /* Make array of n elements */
 	    int arr[] = new int[ N ];
 	    int arrCp[] = new int[ N ];
 	
-	 /* Call method sort */
+	 /* Instantiate a sort object */
 	    Sort sort = new Sort();
-	    
+
 	    /* STEP 2: Profiling */
 	    long start, estFastSum, estSlowSum;
 	    estFastSum = 0;
@@ -44,6 +44,12 @@ public class Assignment {
 	    float avgSlow = estSlowSum / AVERAGEOVER;
 	    float avgFast = estFastSum / AVERAGEOVER;
 	    
-	    System.out.printf("%f\t%f\n", avgSlow, avgFast);
+	    System.out.printf("%d\t%f\t%f\n", N, avgSlow, avgFast);
 	}    
+	
+	public static void printArray(int arr[]){
+	    for (int i = 0; i < arr.length; i++)
+	        System.out.print(arr[i]+" ");            
+	        System.out.println("\n");
+	}
 }
