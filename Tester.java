6a2b9e691097da68fbf964acc54dbdd2d98e5ece
mazh91 <milad.azh@ryerson.ca>
2017-10-12 00:06:32 -0400

    provide input validation

--- Tester.java
+++ Tester.java
@@ -6,10 +6,20 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	    Random random = new Random(8415);
-		for(String arg: args){
-			int N = Integer.parseInt(arg);
-			new Assignment(N, random);
+	    if (args.length < 3){
+	    	System.err.println("Invalid number of number of arguments!");
+	    	System.exit(-1);
+	    }
+	    try{
+			for(String arg: args){
+				int N = Integer.parseInt(arg);
+				new Assignment(N, random);
+			}
 		}
+	    catch(NumberFormatException e){
+	    	System.err.println("Invalid input: expected integer values");
+	    	System.exit(-1);
+	    }
 	}
 
 }
